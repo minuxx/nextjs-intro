e9e5f3b026ab5eb8364bb101b35a4b9dde18c0a0
minuxx <minux8905@gmail.com>
2023-03-10 17:16:43 +0900

    done #1.4 CSS Modules

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,28 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import styles from './NavBar.module.css'
 
 export default function NavBar() {
   const router = useRouter()
 
   return (
     <nav>
-      <Link href="/">
-        <a>Home</a>
+      <Link
+        href="/"
+        className={`${styles.link} ${
+          router.pathname === '/' ? styles.active : ''
+        }`}
+      >
+        Hello
       </Link>
-      <Link href="/about">
-        <a>About</a>
+      <Link
+        href="/about"
+        className={[
+          styles.link,
+          router.pathname === '/about' ? styles.active : '',
+        ].join(' ')}
+      >
+        About
       </Link>
     </nav>
   )
