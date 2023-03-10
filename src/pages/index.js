bb1275bf43feb778153debc91d203ccaa8c2d737
minuxx <minux8905@gmail.com>
2023-03-10 16:57:07 +0900

    done #1.3 Routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
+import NavBar from '@/components/NavBar'
+import { useState } from 'react'
+
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <h1>Hello</h1>
     </div>
   )
