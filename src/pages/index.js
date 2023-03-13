8480a2b6344ac9488c9d1eb2fb76460c0865218d
minuxx <minux8905@gmail.com>
2023-03-13 11:14:53 +0900

    done #2.0 Patterns

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
+import Head from 'next/head'
 import NavBar from '@/components/NavBar'
 import { useState } from 'react'
+import Seo from '@/components/Seo'
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>Hello</h1>
     </div>
   )
