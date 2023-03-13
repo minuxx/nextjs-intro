e49c66443d13c3148607d006702f8e828fc344d7
minuxx <minux8905@gmail.com>
2023-03-13 11:59:15 +0900

    done #2.1 Fetching Data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import Head from 'next/head'
 import NavBar from '@/components/NavBar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Seo from '@/components/Seo'
 
+const API_KEY = 'ed8f5e6c3b01b09d1cf884f7315fc7de'
+const BASE_URL = 'https://api.themoviedb.org/3'
+
 export default function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const { results } = await (
+        await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
