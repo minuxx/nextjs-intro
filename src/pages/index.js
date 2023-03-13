59a5f70afa23d0a49dd2f82b70e40a9fca31de9b
minuxx <minux8905@gmail.com>
2023-03-13 14:46:18 +0900

    done #2.7 Catch All

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,7 @@ import { useRouter } from 'next/router'
 export default function Home({ results }) {
   const router = useRouter()
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    )
+    router.push(`/movies/${title}/${id}`)
   }
 
   return (
@@ -32,13 +24,7 @@ export default function Home({ results }) {
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>
             <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
+              href={`/movies/${movie.original_title}/${movie.id}`}
               legacyBehavior
             >
               <a>{movie.original_title}</a>
---
+++ src/pages/movies/[...params].js
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+import Seo from '../../components/Seo'
+
+export default function Detail({ params }) {
+  const router = useRouter()
+  console.log(router)
+  const [title, id] = params || []
+
+  return (
+    <div>
+      <Seo title={title} />
+      <h4>{title || 'Loading...'}</h4>
+    </div>
+  )
+}
+
+export function getServerSideProps({ query: { params } }) {
+  return {
+    props: {
+      params,
+    },
+  }
+}
